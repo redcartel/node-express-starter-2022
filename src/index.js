efc09d49b2908820c2dabb47b9c9fe4742b6cbee
Carter Adams <carteradams@protonmail.com>
2022-04-12 15:15:40 -0400

    bit of es6 syntax

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res)=>{
     res.json({'message': 'hello'});
 })
 
-app.listen(3000, ()=>{
+app.listen(process.env['PORT'] ?? 3000, ()=>{
     console.log('listening on 3000');
 })
