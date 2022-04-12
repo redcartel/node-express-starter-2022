85631fdfb0cc37c2d2dfd5347066fa70692b8dec
Carter Adams <carteradams@protonmail.com>
2022-04-12 16:09:26 -0400

    report NODE_ENV in server output

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 import app from './app.js';
 
-
-
 app.listen(process.env['PORT'] ?? 3000, ()=>{
-    console.log('listening on 3000');
+    console.log(`listening on 3000 with NODE_ENV=${process.env['NODE_ENV']}`);
 })
