8205b6559bb5945b6c6c8d045c30f7dacf298ece
Carter Adams <carteradams@protonmail.com>
2022-04-12 20:49:20 -0400

    default environment to production

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     port: parseInt(process.env['PORT'] ?? 3000),
-    nodeEnv: process.env['NODE_ENV'] ?? 'development'
+    nodeEnv: process.env['NODE_ENV'] ?? 'production'
 }
 
 export default config;
