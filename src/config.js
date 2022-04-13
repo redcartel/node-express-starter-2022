9f3ea83f78b09c9abc546fea2bfdc1777cb20747
Carter Adams <carteradams@protonmail.com>
2022-04-13 16:00:05 -0400

    docstring in config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,12 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
+/**
+ * Pattern for config is:
+ * key: proces.env['KEY'] ?? default
+ */
 const config = {
-    port: parseInt(process.env['PORT'] ?? 3000),
+    port: process.env['PORT'] ?? 3000,
     nodeEnv: process.env['NODE_ENV'] ?? 'production'
 }
 
