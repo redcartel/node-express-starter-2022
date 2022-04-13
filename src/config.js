191e81abb2278afb9761d2a264280bbc5a13f3f5
Carter Adams <carteradams@protonmail.com>
2022-04-13 14:38:49 -0400

    babel reloading on .env updates

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 const config = {
     port: parseInt(process.env['PORT'] ?? 3000),
     nodeEnv: process.env['NODE_ENV'] ?? 'production'
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import dotenv from 'dotenv';
-dotenv.config();
+
 import config from 'src/config';
 import app from 'src/app';
 
